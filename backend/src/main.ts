affe5678616f182a965486c9d6ae891719ea9a49
bvhithesh012 <bvhithesh30@gmail.com>
2026-08-19 20:04:38 +0530

    Configure backend for production hosting

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -17,8 +17,8 @@ async function bootstrap() {
       transform: true,
     }),
   );
-
-  await app.listen(process.env.PORT ?? 4000, '0.0.0.0');
+  const port = process.env.PORT || 4000;
+  await app.listen(port,'0.0.0.0');
 }
 
 bootstrap();
